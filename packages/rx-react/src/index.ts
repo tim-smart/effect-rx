704b836a5641df7c216b41b715dbd04c998609ef
Tim <hello@timsmart.co>
2023-12-18 15:55:34 +1300

    update effect

--- packages/rx-react/src/index.ts
+++ packages/rx-react/src/index.ts
@@ -40,6 +40,7 @@ export const RegistryContext = React.createContext<Registry.Registry>(Registry.m
 interface RxStore<A> {
   readonly subscribe: (f: () => void) => () => void
   readonly snapshot: () => A
+  readonly serverSnapshot: () => A
 }
 
 const storeRegistry = globalValue(
@@ -59,6 +60,9 @@ function makeStore<A>(registry: Registry.Registry, rx: Rx.Rx<A>): RxStore<A> {
     },
     snapshot() {
       return registry.get(rx)
+    },
+    serverSnapshot() {
+      return undefined as any
     }
   }
   stores.set(rx, newStore)
@@ -67,9 +71,7 @@ function makeStore<A>(registry: Registry.Registry, rx: Rx.Rx<A>): RxStore<A> {
 
 function useStore<A>(registry: Registry.Registry, rx: Rx.Rx<A>): A {
   const store = makeStore(registry, rx)
-  return React.useSyncExternalStore(store.subscribe, store.snapshot, () => {
-    return undefined as A
-  })
+  return React.useSyncExternalStore(store.subscribe, store.snapshot, store.serverSnapshot)
 }
 
 /**
