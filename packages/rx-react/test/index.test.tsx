419e37cf60e8b2f05941c349697752ddeeaf7479
Ethan Niser <ethan@niser.com>
2025-07-20 19:27:08 -0700

    add suspense tests

--- packages/rx-react/test/index.test.tsx
+++ packages/rx-react/test/index.test.tsx
@@ -1,8 +1,11 @@
 import * as Registry from "@effect-rx/rx/Registry"
 import * as Rx from "@effect-rx/rx/Rx"
 import { act, render, screen, waitFor } from "@testing-library/react"
+import { Effect } from "effect"
+import { Suspense } from "react"
+import { ErrorBoundary } from "react-error-boundary"
 import { beforeEach, describe, expect, test } from "vitest"
-import { RegistryContext, useRxValue } from "../src/index.js"
+import { RegistryContext, useRxSuspenseSuccess, useRxValue } from "../src/index.js"
 
 describe("rx-react", () => {
   let registry: Registry.Registry
@@ -88,5 +91,48 @@ describe("rx-react", () => {
 
       expect(screen.getByTestId("value")).toHaveTextContent("20")
     })
+
+    test("suspense success", () => {
+      const rx = Rx.make(Effect.never)
+
+      function TestComponent() {
+        const value = useRxSuspenseSuccess(rx).value
+        return <div data-testid="value">{value}</div>
+      }
+
+      render(
+        <RegistryContext.Provider value={registry}>
+          <Suspense fallback={<div data-testid="loading">Loading...</div>}>
+            <TestComponent />
+          </Suspense>
+        </RegistryContext.Provider>
+      )
+
+      expect(screen.getByTestId("loading")).toBeInTheDocument()
+    })
+  })
+
+  test("suspense error", () => {
+    const rx = Rx.make(Effect.fail(new Error("test")))
+    function TestComponent() {
+      const value = useRxSuspenseSuccess(rx).value
+      return <div data-testid="value">{value}</div>
+    }
+
+    render(
+      <RegistryContext.Provider value={registry}>
+        <ErrorBoundary fallback={<div data-testid="error">Error</div>}>
+          <Suspense fallback={<div data-testid="loading">Loading...</div>}>
+            <TestComponent />
+          </Suspense>
+        </ErrorBoundary>
+      </RegistryContext.Provider>,
+      {
+        // dont log error to console (the default behavior)
+        onCaughtError: () => {}
+      }
+    )
+
+    expect(screen.getByTestId("error")).toBeInTheDocument()
   })
 })
